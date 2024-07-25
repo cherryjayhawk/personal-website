876c474a56c2a3c66281e8cb43c661a9c6dc4e3e
cherryjayhawk <muhbintangr19@gmail.com>
2024-07-26 01:50:48 +0700

    add metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
 import LandingPage from "@/components/section/landing-page";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Home - Mubirastudio',
+  description: 'A Software Engineer based in Bandung, Indonesia. See for more information about me.',
+}
 
 export default function Home() {
   return (
