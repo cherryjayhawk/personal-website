b470361707832c3487d9c93b95c9859a940df197
cherryjayhawk <muhbintangr19@gmail.com>
2024-07-26 23:35:04 +0700

    add section component page container

--- components/section/page-container.tsx
+++ components/section/page-container.tsx
@@ -1,8 +1,9 @@
+"use client"
 import { ReactNode } from 'react'
 
 function PageContainer({ children }: { children: ReactNode}) {
     return (
-        <div className="flex min-h-dvh w-dvw max-w-7xl pt-24 mx-auto text-foreground">
+        <div className="flex flex-col min-h-dvh w-dvw max-w-7xl pt-24 mx-auto text-foreground">
             { children }
         </div>
     )
