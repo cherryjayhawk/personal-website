fe13879b8e6f36baabaafb9b40a120585ffbbfd2
cherryjayhawk <muhbintangr19@gmail.com>
2024-07-29 13:59:46 +0700

    add component section project

--- components/section/project-section.tsx
+++ components/section/project-section.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react"
 
 function ProjectSection({ children }: { children: ReactNode }) {
   return (
-    <div className="flex justify-center items-center min-h-dvh w-full">
+    <div className="flex justify-center items-center min-h-dvh w-full z-10">
         { children }
     </div>
   )
