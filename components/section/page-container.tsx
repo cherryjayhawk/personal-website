681589a6fb42aa8879608e7a595490bbf5c18ad2
cherryjayhawk <muhbintangr19@gmail.com>
2024-07-29 13:58:09 +0700

    add grid background

--- components/section/page-container.tsx
+++ components/section/page-container.tsx
@@ -3,8 +3,11 @@ import { ReactNode } from 'react'
 
 function PageContainer({ children }: { children: ReactNode}) {
     return (
-        <div className="flex flex-col min-h-dvh w-dvw max-w-7xl pt-24 mx-auto text-foreground">
-            { children }
+        <div className="min-h-dvh w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+            <div className="flex flex-col min-h-dvh w-dvw max-w-7xl mx-auto text-foreground">
+                { children }
+            </div>
+            <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_10%,black)]"></div>
         </div>
     )
 }
