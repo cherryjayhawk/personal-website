f33e93b9424d073c4b87f3bd868ea6ebc676826e
cherryjayhawk <muhbintangr19@gmail.com>
2025-11-03 14:58:31 +0700

    update linkpreview to nextjs link

--- components/ui/landing-page-text.tsx
+++ components/ui/landing-page-text.tsx
@@ -1,24 +1,27 @@
-"use client";
-import { LinkPreview } from "@/components/ui/link-preview";
+// "use client";
+// import { LinkPreview } from "@/components/ui/link-preview";
+import Link from "next/link";
 
 export function LandingPageText() {
   return (
     <div className="flex justify-center items-start flex-col px-4">
       <p className="text-neutral-500 dark:text-neutral-400 text-sm md:text-base max-w-3xl  text-left mb-10">
         Visit{" "}
-        <LinkPreview
-          url="https://mubirastudio.vercel.app/about"
+        <Link
+          // url="https://mubirastudio.vercel.app/about"
+          href={"/about"}
           className="font-bold bg-clip-text text-transparent bg-gradient-to-br from-purple-500 to-pink-500"
         >
           About Me
-        </LinkPreview>{" "}
+        </Link>{" "}
         or{" "}
-        <LinkPreview
-          url="https://mubirastudio.vercel.app/about"
+        <Link
+          // url="https://mubirastudio.vercel.app/projects"
+          href={"/projects"}
           className="font-bold bg-clip-text text-transparent bg-gradient-to-br from-purple-500 to-pink-500"
         >
           My Projects
-        </LinkPreview>{" "}
+        </Link>{" "}
         to learn more.
       </p>
     </div>
